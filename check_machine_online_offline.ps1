e742e230986be0fc286803c40fb130cb7e2037e2
sandeep-sr <32605591+sandeep-sr@users.noreply.github.com>
2017-10-09 11:56:29 +0530

    Add files via upload
    simple powershell script to say machine is offline / online

---
+++ check_machine_online_offline.ps1
@@ -0,0 +1,22 @@
+﻿#Author : Sandeep S R
+#Simple script to check whether the server is online or offline
+#copy server hotname/ip in missing.txt
+
+$servers=Get-Content C:\temp\missing.txt
+
+foreach($server in $servers)
+{
+
+$ts=Test-Connection -ComputerName $server -Count 3 -Quiet
+
+if($ts -eq "True"){
+Write-Output "$server - online"
+}
+
+else
+{
+Write-Output "$server - offline"
+}
+
+
+}
