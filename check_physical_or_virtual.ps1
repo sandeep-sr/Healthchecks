d7780eb94131431d322c220b28d48e437a757db0
sandeep-sr <32605591+sandeep-sr@users.noreply.github.com>
2017-10-09 19:41:20 +0530

    Add files via upload
    checks if server is a virtual or physical machine

---
+++ check_physical_or_virtual.ps1
@@ -0,0 +1,18 @@
+﻿
+$servers=Get-Content C:\scripts\serverlist.txt
+
+foreach ($server in $servers)
+{
+
+$model=(Get-WmiObject -Class win32_computersystem -ComputerName $server).model
+
+if($model.toupper().contains("VIRTUAL"))
+      {
+            write-output "$server is Virtual"
+      }
+      Else
+      {
+            write-output "$server is Physical"
+      }
+
+}
