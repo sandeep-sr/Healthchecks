b42300b8deea4eec0ab368763ae2e857e017c1e3
sandeep-sr <32605591+sandeep-sr@users.noreply.github.com>
2017-10-09 19:36:42 +0530

    Add files via upload
    get the IE - internet explorer version from bunch of machines

---
+++ Find_IE_Version.ps1
@@ -0,0 +1,28 @@
+﻿#Author : Sandeep S R
+#Script can be used to find IE version from machines 
+#IE versions numbering sits in different places in registry else it became bit bigger 
+
+$servers=Get-Content C:\scripts\serverlist.txt
+ 
+foreach( $server in  $servers){
+ 
+$key=invoke-command -computername $server {get-itemproperty -Path "HKLM:SOFTWARE\Microsoft\Internet Explorer"}
+ 
+if($key -match "svcVersion"){ 
+
+$version=Invoke-Command -ComputerName $server {(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Internet Explorer").svcVersion}
+$versplit=$version.split(".")[0]
+write-output " $server - IE $versplit "
+
+ } 
+ 
+else {
+
+$version=Invoke-Command -ComputerName $server {(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Internet Explorer").Version}
+$versplit=$version.split(".")[0]
+write-output " $server - IE $versplit " 
+
+ }
+ 
+} 
+
