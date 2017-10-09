04da5da5a714afd92a55bd65859d1828ac7fd76f
sandeep-sr <32605591+sandeep-sr@users.noreply.github.com>
2017-10-09 17:37:51 +0530

    Add files via upload
    script written in powershell- uses to find patches/scripts installed in 'N' number of days .
    can be used prior to patching to troubleshoot issues arises after it

---
+++ patches_installed_in_number_of_days.ps1
@@ -0,0 +1,17 @@
+﻿#Author : Sandeep S R
+#scripts get the hotfixs installed in a machine between DateA & DateB
+#once the script executed it creates a file with server name in temp folder with details
+#place the list of servers in serverlist.txt
+#As an example i have made 35days prior
+
+$DateA=(get-date).AddDays(-35)
+$DateB=Get-Date
+$servers=Get-Content C:\serverlist.txt
+foreach($server in $servers){
+$hfix=Get-HotFix -ComputerName $server|Where {$_.InstalledOn -gt $DateA -AND $_.InstalledOn -lt $DateB } | sort InstalledOn
+if($hfix -eq $null){Write-Output "No updates installed in last 7 days" | out-file C:\temp\$server.txt}
+else{Write-Output "$hfix"
+$hfix|out-file C:\temp\$server.txt}
+
+}
+
